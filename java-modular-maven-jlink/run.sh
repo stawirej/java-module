10f1294ae8aeb31ed33bd7273dcdff56464ada1d
Piotr Stawirej <piotr.stawirej@gmail.com>
2018-04-05 12:47:22 +0200

    Rename jlink module

--- java-modular-maven-jlink/run.sh
+++ java-modular-maven-jlink/run.sh
@@ -2,4 +2,4 @@ JAVA_HOME="c:\\Program Files\\Java\\jdk-10\\" mvn clean install
 cd elon
 JAVA_HOME="c:\\Program Files\\Java\\jdk-10\\" mvn exec:exec
 echo "Executing runtime image"
-../mod-jlink/target/maven-jlink/bin/java user.elon.Elon
+../image/target/maven-jlink/bin/java user.elon.Elon
